d6132ec2a0dd6a29f5227950ce9e563313fc54f0
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-10 23:28:03 +0600

    add Onclick Handler Directly Or Via Javascript

---
+++ events.js
@@ -0,0 +1,12 @@
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+let makeBlueBtn = document.getElementById('makeBlue');
+
+makeBlueBtn.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
