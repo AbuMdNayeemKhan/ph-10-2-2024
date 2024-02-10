f2f106d41ed46924ba45869cbe99dc664c3a2e9a
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-10 23:43:00 +0600

    different ways to use add even listener on a button

--- events.js
+++ events.js
@@ -10,3 +10,14 @@ function makeBlue(){
     document.body.style.backgroundColor = 'blue';
 }
 
+let pinkBtn = document.getElementById('makePink');
+
+// option four
+pinkBtn.addEventListener('click', function makePink(){
+    document.body.style.backgroundColor = 'pink';
+});
+
+//final option
+document.getElementById('makeOrange').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'orange';
+});
